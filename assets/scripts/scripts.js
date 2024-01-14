4e9893e2a7e500d720e38847865be1e3206a43cc
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-14 12:53:15 +0100

    IconVolume

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,7 @@
-const scrollContainers = document.querySelectorAll('.container');
+const scrollContainers = document.querySelectorAll('.container')
+const iconVolumeStatus = document.getElementById('volumeStatus')
+const volumeButton = document.getElementById('volumeButton')
+const previewFilm = document.getElementById('previewFilm')
 
 let isDown = false;
 let startX;
@@ -8,25 +11,25 @@ scrollContainers.forEach(scrollContainer => {
     // Eventi del mouse
     scrollContainer.addEventListener('mousedown', (e) => {
         isDown = true;
-        scrollContainer.classList.add('active');
-        startX = e.pageX - scrollContainer.offsetLeft;
-        scrollLeft = scrollContainer.scrollLeft;
+        scrollContainer.classList.add('active')
+        startX = e.pageX - scrollContainer.offsetLeft
+        scrollLeft = scrollContainer.scrollLeft
     });
     scrollContainer.addEventListener('mouseleave', () => {
         isDown = false;
-        scrollContainer.classList.remove('active');
-    });
+        scrollContainer.classList.remove('active')
+    })
     scrollContainer.addEventListener('mouseup', () => {
-        isDown = false;
-        scrollContainer.classList.remove('active');
-    });
+        isDown = false
+        scrollContainer.classList.remove('active')
+    })
     scrollContainer.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - scrollContainer.offsetLeft;
-        const walk = (x - startX) * 3;
-        scrollContainer.scrollLeft = scrollLeft - walk;
-    });
+        if (!isDown) return
+        e.preventDefault()
+        const x = e.pageX - scrollContainer.offsetLeft
+        const walk = (x - startX) * 3
+        scrollContainer.scrollLeft = scrollLeft - walk
+    })
 
     // Eventi touch
     scrollContainer.addEventListener('touchstart', (e) => {
@@ -51,3 +54,20 @@ scrollContainers.forEach(scrollContainer => {
         scrollContainer.scrollLeft = scrollLeft - walk;
     });
 });
+
+
+const cambiaIconaVolume = function () {
+    if (iconVolumeStatus.classList[1] === "bi-volume-mute-fill") {
+        iconVolumeStatus.classList.remove("bi-volume-mute-fill")
+        iconVolumeStatus.classList.add("bi-volume-up-fill")
+    } else {
+        if (iconVolumeStatus.classList[1] === "bi-volume-up-fill") {
+            iconVolumeStatus.classList.remove("bi-volume-up-fill")
+            iconVolumeStatus.classList.add("bi-volume-mute-fill")
+        }
+    }
+}
+
+const tastoVolumePremuto = function () {
+    cambiaIconaVolume()
+}
